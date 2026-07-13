5b3d68c6f21a08b30ef672e362fbd4226d9f954b
Gab <gabryella.rodrigues@aluno.uece.br>
2026-07-13 13:59:00 -0300

    Atualizando deploy

--- webpalooza/src/Rooter.js
+++ webpalooza/src/Rooter.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Loading from './Loading';
 
